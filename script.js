543f75321c18ccbaaecceb0ecd5407163a5bc335
jungj0925 <86926925+jungj0925@users.noreply.github.com>
2021-08-02 19:58:21 -0400

    updating files

--- script.js
+++ script.js
@@ -10,3 +10,25 @@ links.addEventListener('click', () => {
 	navbarLinks.classList.toggle('active')
 })
 
+var form = document.getElementById("my-form");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+  	status.classList.add('success');
+    status.innerHTML = "Message sent successfully &#x2713;";
+    form.reset()
+  }).catch(error => {
+  	status.classList.add('success');
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+}
+form.addEventListener("submit", handleSubmit)
